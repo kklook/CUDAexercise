044db8046f8538582d16314de8ed14b7bf64fffd
null122 <714779883@qq.com>
2016-09-16 16:52:29 +0800

    use texture but don't know why it's trun slow

--- conduction.cu
+++ conduction.cu
@@ -10,6 +10,10 @@
 #define MIN_TEMP 0.0001f
 #define SPEED 0.25f
 
+texture<float, 2> texConstSrc;
+texture<float, 2> texIn;
+texture<float, 2> texOut;
+
 struct DataBlock 
 {
 	unsigned char * output_bitmap;
@@ -23,37 +27,38 @@ struct DataBlock
 	float frames;
 };
 
-__global__ void copy_const_kernel( float * iptr, const float * cptr)
+__global__ void copy_const_kernel( float * iptr)
 {
 	int x = threadIdx.x + blockIdx.x * blockDim.x;
 	int y = threadIdx.y + blockIdx.y * blockDim.y;
 	int offset = x + y * blockDim.x * gridDim.x;
 
-	if (cptr[offset] != 0)
-		iptr[offset] = cptr[offset];
+	float c = tex2D(texConstSrc, x, y);
+	if (c != 0)
+		iptr[offset] = c;
 }
 
-__global__ void blend_kernel( float * outSrc, const float * inSrc )
+__global__ void blend_kernel( float * dst, bool dstOut )
 {
 	int x = threadIdx.x + blockIdx.x * blockDim.x;
 	int y = threadIdx.y + blockIdx.y * blockDim.y;
 	int offset = x + y * blockDim.x * gridDim.x;
-
-	int left = offset - 1;
-	int right = offset + 1;
-	if (x == 0)
-		left++;
-	if (x == DIM - 1)
-		right--;
-
-	int top = offset - DIM;
-	int bottom = offset + DIM;
-	if (y == 0)
-		top += DIM;
-	if (y == DIM - 1)
-		bottom -= DIM;
-
-	outSrc[offset] = inSrc[offset] + SPEED * (inSrc[top] + inSrc[bottom] + inSrc[left] + inSrc[right] - inSrc[offset] * 4);
+	
+	float t, l, c, r, b;
+	if (dstOut) {
+		t = tex2D(texIn, x, y - 1);
+		l = tex2D(texIn, x - 1, y);
+		c = tex2D(texIn, x, y);
+		r = tex2D(texIn, x + 1, y);
+		b = tex2D(texIn, x, y + 1);
+	} else {
+		t = tex2D(texOut, x, y - 1);
+		l = tex2D(texOut, x - 1, y);
+		c = tex2D(texOut, x, y);
+		r = tex2D(texOut, x + 1, y);
+		b = tex2D(texOut, x, y + 1);
+	}
+	dst[offset] = c + SPEED * (t + b + r + l - 4 * c);
 }
 
 void anim_gpu( DataBlock * d, int ticks )
@@ -62,10 +67,20 @@ void anim_gpu( DataBlock * d, int ticks )
 	dim3 blocks(DIM / 16, DIM / 16);
 	dim3 threads(16, 16);
 	CPUAnimBitmap * bitmap = d->bitmap;
+
+	volatile bool dstOut = true;
 	for (int i = 0; i < 90; i++) {
-		copy_const_kernel<<<blocks, threads>>>( d->dev_inSrc, d->dev_constSrc );
-		blend_kernel<<<blocks, threads>>>( d->dev_outSrc, d->dev_inSrc );
-		swap( d->dev_inSrc, d->dev_outSrc );
+		float * in, * out;
+		if (dstOut) {
+			in = d->dev_inSrc;
+			out = d->dev_outSrc;
+		} else {
+			in = d->dev_outSrc;
+			out = d->dev_inSrc;
+		}
+		copy_const_kernel<<<blocks, threads>>>( in );
+		blend_kernel<<<blocks, threads>>>( out, dstOut );
+		dstOut = !dstOut;
 	}
 	float_to_color<<<blocks, threads>>>( d->output_bitmap, d->dev_inSrc );
 	HANDLE_ERROR( cudaMemcpy( bitmap->get_ptr(), d->output_bitmap, bitmap->image_size(), cudaMemcpyDeviceToHost ) );
@@ -81,6 +96,9 @@ void anim_gpu( DataBlock * d, int ticks )
 
 void anim_exit( DataBlock * d )
 {
+	cudaUnbindTexture( texIn );
+	cudaUnbindTexture( texOut );
+	cudaUnbindTexture( texConstSrc );
 	cudaFree( d->dev_inSrc );
 	cudaFree( d->dev_outSrc );
 	cudaFree( d->dev_constSrc );
@@ -98,12 +116,18 @@ int main(void)
 	data.frames = 0;
 	HANDLE_ERROR( cudaEventCreate( &data.start ) );
 	HANDLE_ERROR( cudaEventCreate( &data.stop ) );
+	int imageSize = bitmap.image_size();
 
 	HANDLE_ERROR( cudaMalloc( (void **)&data.output_bitmap, bitmap.image_size() ) );
 
 	HANDLE_ERROR( cudaMalloc( (void **)&data.dev_inSrc, bitmap.image_size() ) );
 	HANDLE_ERROR( cudaMalloc( (void **)&data.dev_outSrc, bitmap.image_size() ) );
 	HANDLE_ERROR( cudaMalloc( (void **)&data.dev_constSrc, bitmap.image_size() ) );
+
+	cudaChannelFormatDesc desc = cudaCreateChannelDesc<float>();
+	HANDLE_ERROR( cudaBindTexture2D( nullptr, texConstSrc, data.dev_constSrc, desc, DIM, DIM, sizeof(float) * DIM ) );
+	HANDLE_ERROR( cudaBindTexture2D( nullptr, texIn, data.dev_inSrc, desc, DIM, DIM, sizeof(float) * DIM ) );
+	HANDLE_ERROR( cudaBindTexture2D( nullptr, texOut, data.dev_outSrc, desc, DIM, DIM, sizeof(float) * DIM ) );
 
 	float * temp = (float *)malloc( bitmap.image_size() );
 	for (int i = 0; i < DIM * DIM; i++) {
