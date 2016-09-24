2ec23b2e2d47ceb511089ed51b49b9665efca4ba
null122 <714779883@qq.com>
2016-09-24 15:55:00 +0800

    add shared memory fast version

--- Histogram.cu
+++ Histogram.cu
@@ -11,12 +11,17 @@ __global__ void histo_kernel( unsigned char * buffer,
 							  long size,
 							  unsigned int * histo) 
 {
+	__shared__ unsigned int temp[256];
+	temp[threadIdx.x] = 0;
+	__syncthreads();
 	int i = threadIdx.x + blockIdx.x * blockDim.x;
 	int stride = blockDim.x * gridDim.x;
 	while (i < size) {
-		atomicAdd( &(histo[buffer[i]]), 1 );
+		atomicAdd( &(temp[buffer[i]]), 1 );
 		i += stride;
 	}
+	__syncthreads();
+	atomicAdd( &(histo[threadIdx.x]), temp[threadIdx.x] );
 }
 
 int main(void)
